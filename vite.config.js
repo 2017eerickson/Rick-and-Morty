021e36734817ef5d4af9f9bec9165daebc9edbcc
ericka <2017eerickson@gmail.com>
2026-03-03 06:33:14 -0500

    edited package locka nd vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-apps/rickAndMorty',
+  base: '/Rick-and-Morty',
   plugins: [
     react(),
     tailwindcss()
